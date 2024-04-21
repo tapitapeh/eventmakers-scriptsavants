386a34d9eb50d9da77ca5347f66e5013120c825e
Achan <tapitapeh@gmail.com>
2024-04-21 22:37:18 +0800

    implement join participant in detail page

--- src/app/event/[slug]/page.jsx
+++ src/app/event/[slug]/page.jsx
@@ -5,15 +5,17 @@ import Link from "next/link";
   TODO:
   - [x] Implement get event API
   - [ ] Implement edit/delete if author is the same as the logged in user (show buttons, implement the APIs)
-  - [ ] Implement join event if the user is not the author (show button, implement the API)
-  - [ ] Implement show list of participants
-  - [ ] Enhance the UI
+  - [x] Implement join event if the user is not the author (show button, implement the API)
+  - [x] Implement show list of participants
+  - [x] Enhance the UI
    */
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 export default function Page({ params }) {
   const [isAuthor, setIsAuthor] = useState(false);
   const [event, setEvent] = useState({});
+  const [participants, setParticipants] = useState([]);
+  const joinFormRef = useRef(null);
 
   async function getEvent() {
     // Get Event API
@@ -23,6 +25,7 @@ export default function Page({ params }) {
     const { data } = await res.json();
     const _event = data.events;
     setEvent(_event);
+    setParticipants(data.participants);
 
     // Check if the author is the same as the logged in user
     const userData = JSON.parse(localStorage.getItem("userData"));
@@ -49,7 +52,35 @@ export default function Page({ params }) {
 
       // TODO : Redirect to home after delete
     } else {
-      // Do nothing!
+      // Do nothing
+    }
+  }
+
+  async function joinEvent(formData) {
+    const data = Object.fromEntries(formData);
+
+    // Join Event API
+    const res = await fetch(
+      `https://eventmakers.devscale.id/events/${params.slug}/join`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    const jsonRes = await res.json();
+
+    if (res.status === 201) {
+      alert(jsonRes.message);
+      joinFormRef.current.reset();
+      document.getElementById("join-modal").close();
+
+      getEvent(); // reload the event data
+    } else {
+      alert(jsonRes.message);
     }
   }
 
@@ -71,7 +102,7 @@ export default function Page({ params }) {
           <div className="space-y-5 w-full">
             <div className="flex justify-between">
               <h1 className="text-3xl font-bold">{event.title}</h1>
-              {isAuthor && (
+              {isAuthor ? (
                 <div className="flex gap-2">
                   <Link href={`/event/${params.slug}/edit`}>
                     <button className="btn">Edit</button>
@@ -80,16 +111,135 @@ export default function Page({ params }) {
                     Delete
                   </button>
                 </div>
+              ) : (
+                <div className="flex gap-2">
+                  <button
+                    className="btn"
+                    onClick={() =>
+                      document.getElementById("join-modal").showModal()
+                    }
+                  >
+                    Join
+                  </button>
+
+                  <dialog id="join-modal" className="modal">
+                    <div className="modal-box">
+                      <h3 className="font-bold text-lg">Join event</h3>
+                      <form method="dialog">
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                          ✕
+                        </button>
+                      </form>
+                      <div>
+                        <form ref={joinFormRef} action={joinEvent}>
+                          <div className="space-y-2 mb-5">
+                            <label className="form-control w-full">
+                              <div className="label">
+                                <span className="label-text">Your name</span>
+                              </div>
+                              <input
+                                name="name"
+                                type="text"
+                                placeholder="Input your name"
+                                className="input input-bordered w-full"
+                                required
+                              />
+                            </label>
+                            <label className="form-control w-full">
+                              <div className="label">
+                                <span className="label-text">Email</span>
+                              </div>
+                              <input
+                                name="email"
+                                type="email"
+                                placeholder="Input your email"
+                                className="input input-bordered w-full"
+                                required
+                              />
+                            </label>
+                            <label className="form-control w-full">
+                              <div className="label">
+                                <span className="label-text">Phone number</span>
+                              </div>
+                              <input
+                                name="phoneNumber"
+                                type="tel"
+                                placeholder="Input your phone number"
+                                className="input input-bordered w-full"
+                                required
+                              />
+                            </label>
+                          </div>
+                          {/* if there is a button in form, it will close the modal */}
+                          <button
+                            type="submit"
+                            className="btn btn-primary w-full mt-5"
+                          >
+                            Join
+                          </button>
+                        </form>
+                      </div>
+                    </div>
+                  </dialog>
+                </div>
               )}
             </div>
             <p className="text-gray-900">{event.dateTime}</p>
 
             {/* participant */}
-            {/* <div>
-            {data.participants.map((participant, index) => {
-              return <div key={participant.id}>{participant.name}</div>;
-            })}
-          </div> */}
+            {participants.length ? (
+              <button
+                className="btn"
+                onClick={() =>
+                  document.getElementById("participants-modal").showModal()
+                }
+              >
+                👥 {participants.length} participant
+                {participants.length > 1 ? "s" : ""}
+              </button>
+            ) : (
+              <p className="text-sm text-gray-600">No participants yet</p>
+            )}
+
+            <dialog id="participants-modal" className="modal">
+              <div className="modal-box">
+                <h3 className="font-bold text-lg">Participants</h3>
+                <form method="dialog">
+                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                    ✕
+                  </button>
+                </form>
+                <div className="space-y-2 mt-5">
+                  {participants.map((participant, index) => {
+                    return (
+                      <div key={participant.id} className="flex items-center">
+                        <div className="avatar placeholder">
+                          <div className="bg-neutral text-neutral-content rounded-full w-8">
+                            <span className="text-xs">
+                              {participant.name.charAt(0).toUpperCase()}
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="ml-3">
+                          <div className="text-bold">{participant.name}</div>
+                          {isAuthor && (
+                            <>
+                              <div className="text-xs text-gray-700">
+                                {participant.email}
+                              </div>
+                              <div className="text-xs text-gray-700">
+                                {participant.phoneNumber}
+                              </div>
+                            </>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </dialog>
             <hr />
             <div>
               <p>{event.description}</p>
