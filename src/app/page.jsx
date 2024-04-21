868e4c7b60e9bc6d9f8ff300a5c26f56f73fc829
Achan <tapitapeh@gmail.com>
2024-04-21 12:04:23 +0800

    basic ui of event list (wip)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,14 @@
-// import { useEffect, useState } from "react";
-
-import Link from "next/link";
+import EventCard from "@/components/EventCard";
 
 export default async function Home() {
   const res = await fetch(`https://eventmakers.devscale.id/events`);
   const { data } = await res.json();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className="btn btn-primary">Button</button>
-
-      <div className="bg-white p-5">
+    <main className="min-h-screen">
+      <div className="grid grid-cols-3 gap-5 bg-white p-5">
         {data.map((detail, index) => {
-          return (
-            <Link href={`/event/${detail.events.id}`} key={index}>
-              <div key={detail.events.id} className="text-black">
-                <div>{detail.events.id}</div>
-                <div>{detail.events.title}</div>
-                <div>{detail.events.description}</div>
-              </div>
-            </Link>
-          );
+          return <EventCard key={index} event={detail.events} />;
         })}
       </div>
     </main>
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,8 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Navbar() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    if (!userData) {
+      setUser({});
+      setLoggedIn(false);
+    } else {
+      setUser(userData);
+      setLoggedIn(true);
+    }
+  }, []);
+
+  function logout() {
+    setUser({});
+    setLoggedIn(false);
+    localStorage.clear();
+
+    router.push("/login");
+  }
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Cari Acara</a>
       </div>
       <div className="flex-none gap-2">
         <div className="form-control">
@@ -12,37 +41,51 @@ export default function Navbar() {
             className="input input-bordered w-24 md:w-auto"
           />
         </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
+        {!loggedIn ? (
+          <div>
+            <Link href="/login">
+              <button className="btn btn-primary">Login</button>
+            </Link>
           </div>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+        ) : (
+          <div className="dropdown dropdown-end">
+            {/* <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full bg-red-500 flex items-center justify-center">
+                <span>{user.name.charAt(0)}</span>
+              </div>
+            </div> */}
+
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar placeholder"
+            >
+              <div className="bg-neutral text-neutral-content rounded-full w-24">
+                <span className="text-xl">
+                  {user.name.charAt(0).toUpperCase()}
+                </span>
+              </div>
+            </div>
+
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">Profile</a>
+              </li>
+              <li>
+                <a className="justify-between" onClick={logout}>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
