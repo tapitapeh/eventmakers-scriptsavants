c50997359b530c6e137dedbe12d4908a4411c540
Achan <tapitapeh@gmail.com>
2024-04-21 20:14:17 +0800

    create my event page

---
+++ src/app/my-events/page.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import EventCard from "@/components/EventCard";
+
+export default function Page() {
+  const [events, setEvents] = useState([]);
+
+  async function getEvents() {
+    // Get Event API
+    const res = await fetch(`https://eventmakers.devscale.id/events`);
+    const { data } = await res.json();
+    setEvents(data);
+  }
+
+  function checkIfAuthor(event) {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    if (userData && event.author === userData.id) {
+      return true;
+    }
+
+    return false;
+  }
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  return (
+    <main className="min-h-screen bg-white">
+      <div className="flex justify-between items-center  mb-5">
+        <h2 className="text-2xl font-bold">My Events</h2>
+
+        <Link href="/event/create">
+          <button className="btn btn-primary">Create event</button>
+        </Link>
+      </div>
+      <div className="grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-5 bg-white">
+        {events.map((detail, index) => {
+          if (checkIfAuthor(detail.events))
+            return <EventCard key={index} event={detail.events} />;
+        })}
+      </div>
+    </main>
+  );
+}
